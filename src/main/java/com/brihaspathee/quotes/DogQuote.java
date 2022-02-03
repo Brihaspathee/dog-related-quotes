19dd618e51525a953ec02f27d2ead891552a00f7
vbalaji215-dev <vbalaji215@outlook.com>
2022-02-02 22:36:39 -0500

    Added DogQuote class

---
+++ src/main/java/com/brihaspathee/quotes/DogQuote.java
@@ -0,0 +1,17 @@
+package com.brihaspathee.quotes;
+
+/**
+ * Created in Intellij IDEA
+ * User: Balaji Varadharajan
+ * Date: 02, February 2022
+ * Time: 10:34 PM
+ * Project: dog-related-quotes
+ * Package Name: com.brihaspathee
+ * To change this template use File | Settings | File and Code Template
+ */
+public class DogQuote {
+
+    public String getDogQuote(){
+        return "This is a dog quote";
+    }
+}
